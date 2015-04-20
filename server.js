eeb9c0428448b5bab03e76f5adaac3733806a891
Erik Griffin <griffin.erik@gmail.com>
2015-04-20 14:58:18 +0100

    Added socket.io

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ var root = __dirname + '/public/';
 app.use(express.static(root));
 
 
+// -- Socket.io --
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  console.log('A user has connected');
+});
+
 
 
 server.listen(3000, function(){
   console.log("Listening on server port 3000");
 });
 
+// +++ Routes +++
 
 app.get('/', function(req,res) {
 
